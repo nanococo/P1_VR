8236ce31db13fbe922e03b4f0ce4359f92642dc8
robertovidal <66501561+robertovidal@users.noreply.github.com>
2022-03-23 21:49:10 -0600

    Adding robot interaction

--- Proyecto Google Cardboard/Assets/Scripts/BotController.cs
+++ Proyecto Google Cardboard/Assets/Scripts/BotController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class BotController : MonoBehaviour
 {
+
+    Animator myAnimator;
+    public AudioSource myAudio;
+    private IEnumerator couroutine;
+    private bool activateTalking;
+    private bool talked;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myAnimator = gameObject.GetComponent<Animator>();
+        activateTalking = false;
+        talked = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (activateTalking){
+            couroutine = startTalking();
+            StartCoroutine(couroutine);
+        }
+    }
+
+    private IEnumerator startTalking(){
+        if (!talked){
+            talked = true;
+            setAnimationTalking();
+            myAudio.Play();
+            yield return new WaitForSeconds(20f);
+            setAnimationIdle();
+        }
+        else
+            yield return new WaitForSeconds(0f);
+    }
+
+    void setAnimationTalking(){
+        myAnimator.SetBool("LookingAtBot", true);
+    }
+
+    void setAnimationIdle(){
+        myAnimator.SetBool("LookingAtBot", false);
+    }
+
+    public void OnPointerEnter(){
+        activateTalking = true;
+    }
+
+    public void OnPointerExit(){
+        activateTalking = false;
     }
 }
